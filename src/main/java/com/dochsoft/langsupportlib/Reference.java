1b818fbeb8432db9b70cb649870ad259d7c825e1
doch13_ <0922happy@naver.com>
2021-07-04 22:05:28 +0900

    Fix: plugin message prefix

--- src/main/java/com/dochsoft/langsupportlib/Reference.java
+++ src/main/java/com/dochsoft/langsupportlib/Reference.java
@@ -4,15 +4,15 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Reference {
-    public static String prefix_normal = "§6[EtereunaCrafting] §r";
-    public static String prefix_opMessage = "§b[EtereunaCrafting-OP메세지] §r";
-    public static String prefix_error = "§c§l[EtereunaCrafting] §7";
+    public static String prefix_normal = "§6[LangSupportLib] §r";
+    public static String prefix_opMessage = "§b[LangSupportLib-OP메세지] §r";
+    public static String prefix_error = "§c§l[LangSupportLib] §7";
 
     public static String COMMAND_CMD_NOT_PRACTICE_MESSAGE = "버킷창에서는 실행되지 않으니, 게임에서 명령어를 실행해주시기 바랍니다";
     public static String COMMAND_NOTALLOW_NOOP_MESSAGE = "관리자 명령어입니다. 명령어를 이용하실려면 관리자 권한을 취득해주세요";
     public static String COMMAND_NOT_CORRECT = "잘못된 명령어입니다";
 
-    public static final String ConfigFolder = "plugins/LangsupportLib/";
+    public static final String ConfigFolder = "plugins/LangSupportLib/";
 
 
 
